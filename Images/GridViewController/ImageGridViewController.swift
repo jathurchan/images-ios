bc37dd3dfb6bdc20f15dd2439581302e3aa6ad12
Jathurchan Selvakumar <admin@jathurchan.com>
2023-11-22 20:51:07 +0100

    Created layout for collection view of ImageGridViewController

--- Images/GridViewController/ImageGridViewController.swift
+++ Images/GridViewController/ImageGridViewController.swift
@@ -15,7 +15,44 @@ class ImageGridViewController: UIViewController {
     }
     
     private func createLayout() -> UICollectionViewLayout {
-        fatalError("not implemented")
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let largeItem = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
+                                                   heightDimension: .fractionalHeight(1.0)))
+            largeItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+
+            let smallItem = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(0.5)))
+            smallItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+            
+            let smallGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
+                                                   heightDimension: .fractionalHeight(1.0)),
+                subitems: [smallItem, smallItem])
+
+            let leadingGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(0.5)),
+                subitems: [largeItem, smallGroup])
+            
+            let trailingGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(0.5)),
+                subitems: [smallGroup, largeItem])
+            
+            let nestedGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(1.0)),
+                subitems: [leadingGroup, trailingGroup])
+            
+            let section = NSCollectionLayoutSection(group: nestedGroup)
+            return section
+        }
+        
+        return layout
     }
     
 }
