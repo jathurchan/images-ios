2f3257626c13217162fe5e9f1458b99612ff3561
Jathurchan Selvakumar <admin@jathurchan.com>
2023-11-24 16:26:34 +0100

    Removed unwanted code

--- Images/GridViewController/ImageGridViewController+Actions.swift
+++ Images/GridViewController/ImageGridViewController+Actions.swift
@@ -2,14 +2,13 @@ import UIKit
 
 extension ImageGridViewController {
     @objc func didPressCancelButton() {
-        print("pressed cancel")
         clearSelectedItems()
         updateToolbarButtons()
     }
     
     
     @objc func didPressDoneButton() {
-        print("pressed done")
+        fatalError()
     }
 }
 
