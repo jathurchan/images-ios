868f0658846746c5ad3c086a1592d31b1716af53
Jathurchan Selvakumar <admin@jathurchan.com>
2023-11-28 09:13:25 +0100

    Delete Images/Models/ImageError.swift

--- Images/Models/ImageError.swift
+++
@@ -1,8 +0,0 @@
-
-import Foundation
-
-enum ImageDataError: Error {
-    case missingData
-    case networkError   // TODO: New case for 429 error: API rate limit exceeded
-    case unexpectedError(Error: Error)
-}
