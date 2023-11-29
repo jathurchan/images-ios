690874df1677c19b31054367fbb21613b2c1f22f
Jathurchan Selvakumar <admin@jathurchan.com>
2023-11-29 17:23:32 +0100

    Added comments to HitError

--- Images/Models/HitError.swift
+++ Images/Models/HitError.swift
@@ -1,7 +1,20 @@
 import Foundation
 
+/// An enumeration that captures errors that may occur when
+/// data is loaded using the Pixabay API and decoded into
+/// an array of `Hit`.
 enum HitError: Error {
+    
+    /// An indication that some expected JSON properties
+    /// were not found when decoding to create a `Hit`.
     case missingData
+    
+    /// An indication that the response received after the
+    /// GET request made using the Pixabay API was
+    /// unsuccessful.
     case networkError
+    
+    /// An indication that the error thrown was not
+    /// expected.
     case unexpectedError(Error: Error)
 }
